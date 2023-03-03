e4eb0ecb13c05c43772d7b3fcd6848d1916c97f9
Nikos <nikosvil@hotmail.com>
2023-03-03 12:58:56 +0200

    Update NS.f

--- halfspace/NS.f
+++ halfspace/NS.f
@@ -1,5 +1,4 @@
-c234567
-	  subroutine NS(q,nx,ny,nz,hx,hy,hz,ci,
+	subroutine NS(q,nx,ny,nz,hx,hy,hz,ci,
      +             pt1,pt2,pt3,pt4,pt5,pt6,pt7,
      +             st1,st2,st3,st4,st5,st6,st7,
      +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,RHS7,
@@ -7,11 +6,11 @@ c234567
      +             a1,a2,a3,b1,b2,b3,c1,c2,c3,sv1,sv2,
      +             tempEZ)
 
-	  implicit none
-      integer :: nx,ny,nz,i
-	  real*8  :: hx,hy,hz,MultVal1(*),MultVal2(*),MultVal3(*),dznrm2,
+	implicit none
+        integer :: nx,ny,nz,i
+	real*8  :: hx,hy,hz,MultVal1(*),MultVal2(*),MultVal3(*),dznrm2,
      +  a1(nx),b1(nx),c1(nx),a2(ny),b2(ny),c2(ny),a3(nz),b3(nz),c3(nz)
-	  complex*16 :: tempEZ((nx-1)*(ny-1)*nz),
+	complex*16 :: tempEZ((nx-1)*(ny-1)*nz),
      +  pt1(nx*(ny-1)*(nz-1)),st1(nx*(ny-1)*(nz-1)),
      +  pt2(nx*ny*(nz-1)),st2(nx*ny*(nz-1)),
      +  pt3(nx*(ny-1)*nz),st3(nx*(ny-1)*nz),
@@ -26,8 +25,10 @@ c234567
      +  sv1(nx*(ny-1)),sv2((nx-1)*ny),
      +  q((nx-1)*ny*(nz-1))
      
+! Subroutine KS computes N*q=tempEZ
+! Input: q((nx-1)*ny*(nz-1))
+! Output: tempEZ((nx-1)*(ny-1)*nz)
 
-c N*EY=tN Evaluation
 	  call B_mult(q,13,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6,pt7)
 	  call S_solve(13,nx,ny,nz,
      +             pt1,pt2,pt3,pt4,pt5,pt6,pt7,
