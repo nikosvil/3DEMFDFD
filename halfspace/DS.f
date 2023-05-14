8d537bfe65104fd3b7ed92ccfc0de9f8bef8989a
Nikos <nikosvil@hotmail.com>
2023-05-14 23:50:15 +0300

    Update DS.f

--- halfspace/DS.f
+++ halfspace/DS.f
@@ -1,7 +1,7 @@
         subroutine DS(q,nx,ny,nz,hx,hy,hz,ci,
-     +              pt1,pt2,pt3,pt4,pt5,pt6,pt7,
-     +              st1,st2,st3,st4,st5,st6,st7,
-     +              RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,RHS7,
+     +              pt1,pt2,pt3,pt4,pt5,pt6,
+     +              st1,st2,st3,st4,st5,st6,
+     +              RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,
      +              MultVal1,MultVal2,MultVal3,
      +              a1,a2,a3,b1,b2,b3,c1,c2,c3,sv1,sv2,
      +              tempEY)
@@ -19,11 +19,9 @@
      +  pt4((nx-1)*ny*(nz-1)),st4((nx-1)*ny*(nz-1)),
      +  pt5((nx-1)*ny*nz),st5((nx-1)*ny*nz),
      +  pt6((nx-1)*(ny-1)*nz),st6((nx-1)*(ny-1)*nz),
-     +  pt7((nx-1)*(ny-1)*(nz-1)),st7((nx-1)*(ny-1)*(nz-1)),
      +  RHS1((2*nx-1)*ny*(nz-1)),RHS2(nx*(2*ny-1)*(nz-1)),
      +  RHS3((2*nx-1)*(ny-1)*nz),RHS4((nx-1)*(2*ny-1)*nz),
      +  RHS5((nx-1)*ny*(2*nz-1)),RHS6(nx*(ny-1)*(2*nz-1)),
-     +  RHS7((nx-1)*(ny-1)*(2*nz-1)),
      +  sv1(nx*(ny-1)),sv2((nx-1)*ny)
 
 ! ===================================================================
@@ -47,19 +45,19 @@
 ! tempEY: complex array, dimension (nx-1)*ny*(nz-1)
 !==================================================================== 
 
-        call B_mult(q,9,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6,pt7)
+        call B_mult(q,9,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6)
         call S_solve(3,nx,ny,nz,
-     +             pt1,pt2,pt3,pt4,pt5,pt6,pt7,
-     +             st1,st2,st3,st4,st5,st6,st7,
-     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,RHS7,
+     +             pt1,pt2,pt3,pt4,pt5,pt6,
+     +             st1,st2,st3,st4,st5,st6,
+     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,
      +             MultVal1,MultVal2,MultVal3,
      +             a1,a2,a3,b1,b2,b3,c1,c2,c3,sv1,sv2)
 
-        call B_mult(pt2,10,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6,pt7)
+        call B_mult(pt2,10,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6)
         call S_solve(10,nx,ny,nz,
-     +             pt1,pt2,pt3,pt4,pt5,pt6,pt7,
-     +             st1,st2,st3,st4,st5,st6,st7,
-     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,RHS7,
+     +             pt1,pt2,pt3,pt4,pt5,pt6,
+     +             st1,st2,st3,st4,st5,st6,
+     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,
      +             MultVal1,MultVal2,MultVal3,
      +             a1,a2,a3,b1,b2,b3,c1,c2,c3,sv1,sv2)
 
