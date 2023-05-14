77f8e8267e9c56a6d7f0f8b2e5e9d2356bf7083d
Nikos <nikosvil@hotmail.com>
2023-05-14 19:31:37 +0300

    Update BS.f

--- halfspace/BS.f
+++ halfspace/BS.f
@@ -26,10 +26,26 @@
      +  sv1(nx*(ny-1)),sv2((nx-1)*ny),
      +  q((nx-1)*ny*(nz-1))
 
-! Subroutine BS computes B*q=tempEX
-! Input: q((nx-1)*ny*(nz-1))
-! Output: tempEX(nx*(ny-1)*(nz-1))
-
+! ===================================================================
+! Title: BS.f 
+! Authors: N. Vilanakis, E. Mathioudakis
+! Details: Applied Mathematics and Computers Lab, Technical University of Crete
+!====================================================================
+! BS.f performs the multiplication B*q=tempEX
+! by performing the operations between terms that make up block B 
+! of matrix U and the input array q
+!====================================================================      
+! Input:
+! q: complex array, dimension (nx-1)*ny*(nz-1)
+! nx: number of elements in x-direction
+! ny: number of elements in y-direction
+! nz: number of elements in z-direction
+! hx: discretization step in x-direction
+! hy: discretization step in y-direction
+! hz: discretization step in z-direction
+! Output:
+! tempEX: complex array, dimension nx*(ny-1)*(nz-1)
+!==================================================================== 
 
         call B_mult(q,1,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6,pt7)
         call S_solve(1,nx,ny,nz,
