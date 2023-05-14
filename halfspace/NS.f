18e73a8f491e06cb475a58ad4d01ae6b15492f2e
Nikos <nikosvil@hotmail.com>
2023-05-14 23:53:05 +0300

    Update NS.f

--- halfspace/NS.f
+++ halfspace/NS.f
@@ -1,7 +1,7 @@
 	subroutine NS(q,nx,ny,nz,hx,hy,hz,ci,
-     +             pt1,pt2,pt3,pt4,pt5,pt6,pt7,
-     +             st1,st2,st3,st4,st5,st6,st7,
-     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,RHS7,
+     +             pt1,pt2,pt3,pt4,pt5,pt6,
+     +             st1,st2,st3,st4,st5,st6,
+     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,
      +             MultVal1,MultVal2,MultVal3,
      +             a1,a2,a3,b1,b2,b3,c1,c2,c3,sv1,sv2,
      +             tempEZ)
@@ -10,18 +10,16 @@
         integer :: nx,ny,nz,i
 	real*8  :: hx,hy,hz,MultVal1(*),MultVal2(*),MultVal3(*),dznrm2,
      +  a1(nx),b1(nx),c1(nx),a2(ny),b2(ny),c2(ny),a3(nz),b3(nz),c3(nz)
-	complex*16 :: tempEZ((nx-1)*(ny-1)*nz),
+	complex*16 :: tempEZ((nx-1)*(ny-1)*nz),ci,
      +  pt1(nx*(ny-1)*(nz-1)),st1(nx*(ny-1)*(nz-1)),
      +  pt2(nx*ny*(nz-1)),st2(nx*ny*(nz-1)),
      +  pt3(nx*(ny-1)*nz),st3(nx*(ny-1)*nz),
      +  pt4((nx-1)*ny*(nz-1)),st4((nx-1)*ny*(nz-1)),
      +  pt5((nx-1)*ny*nz),st5((nx-1)*ny*nz),
      +  pt6((nx-1)*(ny-1)*nz),st6((nx-1)*(ny-1)*nz),
-     +  pt7((nx-1)*(ny-1)*(nz-1)),st7((nx-1)*(ny-1)*(nz-1)),
      +  RHS1((2*nx-1)*ny*(nz-1)),RHS2(nx*(2*ny-1)*(nz-1)), 
      +  RHS3((2*nx-1)*(ny-1)*nz),RHS4((nx-1)*(2*ny-1)*nz),
      +  RHS5((nx-1)*ny*(2*nz-1)),RHS6(nx*(ny-1)*(2*nz-1)),
-     +  RHS7((nx-1)*(ny-1)*(2*nz-1)),ci,
      +  sv1(nx*(ny-1)),sv2((nx-1)*ny),
      +  q((nx-1)*ny*(nz-1))
      
@@ -46,18 +44,18 @@
 ! tempEZ: complex array, dimension (nx-1)*(ny-1)*nz
 !==================================================================== 
 
-	  call B_mult(q,13,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6,pt7)
+	  call B_mult(q,13,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6)
 	  call S_solve(13,nx,ny,nz,
-     +             pt1,pt2,pt3,pt4,pt5,pt6,pt7,
-     +             st1,st2,st3,st4,st5,st6,st7,
-     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,RHS7,
+     +             pt1,pt2,pt3,pt4,pt5,pt6,
+     +             st1,st2,st3,st4,st5,st6,
+     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,
      +             MultVal1,MultVal2,MultVal3,
      +             a1,a2,a3,b1,b2,b3,c1,c2,c3,sv1,sv2)
-	  call B_mult(pt7,20,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6,pt7)
+	  call B_mult(pt7,20,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6)
 	  call S_solve(20,nx,ny,nz,
-     +             pt1,pt2,pt3,pt4,pt5,pt6,pt7,
-     +             st1,st2,st3,st4,st5,st6,st7,
-     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,RHS7,
+     +             pt1,pt2,pt3,pt4,pt5,pt6,
+     +             st1,st2,st3,st4,st5,st6,
+     +             RHS1,RHS2,RHS3,RHS4,RHS5,RHS6,
      +             MultVal1,MultVal2,MultVal3,
      +             a1,a2,a3,b1,b2,b3,c1,c2,c3,sv1,sv2)
 
