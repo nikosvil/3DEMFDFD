e7fbe27e79e104f752fb4fc5afd2b0087fadffb0
Nikos <nikosvil@hotmail.com>
2023-05-15 18:17:00 +0300

    Update DS.f

--- halfspace/DS.f
+++ halfspace/DS.f
@@ -35,20 +35,20 @@
 !====================================================================      
 ! Input:
 ! q: complex array, dimension nx*(ny-1)*(nz-1)
-! nx: number of elements in x-direction
-! ny: number of elements in y-direction
-! nz: number of elements in z-direction
-! hx: discretization step in x-direction
-! hy: discretization step in y-direction
-! hz: discretization step in z-direction
+! nx: integer, number of elements in x-direction
+! ny: integer, number of elements in y-direction
+! nz: integer, number of elements in z-direction
+! hx: real, discretization step in x-direction
+! hy: real, discretization step in y-direction
+! hz: real, discretization step in z-direction
 ! Output:
 ! tempEY: complex array, dimension (nx-1)*ny*(nz-1)
 !==================================================================== 
 
-! B9*q=
+! B9*q=pt2
 !====================================================================
         call B_mult(q,9,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6)
-! S3*=
+! S3*pt2=pt2
 !====================================================================
         call S_solve(3,nx,ny,nz,
      +             pt1,pt2,pt3,pt4,pt5,pt6,
@@ -57,10 +57,10 @@
      +             MultVal1,MultVal2,MultVal3,
      +             a1,a2,a3,b1,b2,b3,c1,c2,c3,sv1,sv2)
      
-! S10*pt2=
+! B10*pt2=pt4
 !====================================================================
         call B_mult(pt2,10,nx,ny,nz,pt1,pt2,pt3,pt4,pt5,pt6)
-! S10*=
+! S10*pt4=pt4
 !====================================================================
         call S_solve(10,nx,ny,nz,
      +             pt1,pt2,pt3,pt4,pt5,pt6,
