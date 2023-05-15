a3dea8252cdf9fd723c8a2baeb50e7e4264b8271
Nikos <nikosvil@hotmail.com>
2023-05-15 22:59:02 +0300

    Update BS.f

--- halfspace/BS.f
+++ halfspace/BS.f
@@ -1,3 +1,23 @@
+! ===================================================================
+! Title: BS.f 
+! Authors: N. Vilanakis, E. Mathioudakis
+! Details: Applied Mathematics and Computers Lab, Technical University of Crete
+!====================================================================
+! BS.f performs the multiplication B*q=tempEX
+! by performing the operations between terms that make up block B 
+! of matrix U and the input array q
+!====================================================================      
+! Input:
+! q: complex array, dimension (nx-1)*ny*(nz-1)
+! nx: integer, number of elements in x-direction
+! ny: integer, number of elements in y-direction
+! nz: integer, number of elements in z-direction
+! hx: real, discretization step in x-direction
+! hy: real, discretization step in y-direction
+! hz: real, discretization step in z-direction
+! Output:
+! tempEX: complex array, dimension nx*(ny-1)*(nz-1)
+!==================================================================== 
         subroutine BS(q,nx,ny,nz,hx,hy,hz,ci,
      +              pt1,pt2,pt3,pt4,pt5,pt6,
      +              st1,st2,st3,st4,st5,st6,
@@ -23,27 +43,6 @@
      +  RHS5((nx-1)*ny*(2*nz-1)),RHS6(nx*(ny-1)*(2*nz-1)),
      +  sv1(nx*(ny-1)),sv2((nx-1)*ny),
      +  q((nx-1)*ny*(nz-1))
-
-! ===================================================================
-! Title: BS.f 
-! Authors: N. Vilanakis, E. Mathioudakis
-! Details: Applied Mathematics and Computers Lab, Technical University of Crete
-!====================================================================
-! BS.f performs the multiplication B*q=tempEX
-! by performing the operations between terms that make up block B 
-! of matrix U and the input array q
-!====================================================================      
-! Input:
-! q: complex array, dimension (nx-1)*ny*(nz-1)
-! nx: integer, number of elements in x-direction
-! ny: integer, number of elements in y-direction
-! nz: integer, number of elements in z-direction
-! hx: real, discretization step in x-direction
-! hy: real, discretization step in y-direction
-! hz: real, discretization step in z-direction
-! Output:
-! tempEX: complex array, dimension nx*(ny-1)*(nz-1)
-!==================================================================== 
 
 ! B1*q=pt2
 !====================================================================
