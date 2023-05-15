a912beebade56737ccdf77f6523e33e73a0ce643
Nikos <nikosvil@hotmail.com>
2023-05-15 19:23:41 +0300

    Update makeRHS.f

--- halfspace/makeRHS.f
+++ halfspace/makeRHS.f
@@ -1,24 +1,3 @@
-      subroutine makeRHS(nx,ny,nz,omega,m0,ci,wpath,
-     +        SIGMAEX,SIGMAEY,SIGMAEZ,SIGMADiffEX,SIGMADiffEY,
-     +        realExBack,imagExBack,realEyBack,imagEyBack,ExBack,EyBack,
-     +        e1,e2,e3,F1,F2)
-
-      implicit none
-      integer:: nx,ny,nz,i
-      character(64) :: wpath
-      real*8:: omega,m0,dznrm2,
-     +          SIGMAEX(nx*(ny-1)*(nz-1)),SIGMADiffEX(nx*(ny-1)*(nz-1)),
-     +          SIGMAEY((nx-1)*ny*(nz-1)),SIGMADiffEY((nx-1)*ny*(nz-1)),
-     +          SIGMAEZ((nx-1)*(ny-1)*nz),
-     +          realExBack(nx*(ny-1)*(nz-1)),
-     +          imagExBack(nx*(ny-1)*(nz-1)),
-     +          realEyBack((nx-1)*ny*(nz-1)),
-     +          imagEyBack((nx-1)*ny*(nz-1))
- 
-      complex*16:: ci,ExBack(nx*(ny-1)*(nz-1)),EyBack((nx-1)*ny*(nz-1)),
-     +  e1(nx*(ny-1)*(nz-1)),e2((nx-1)*ny*(nz-1)),e3((nx-1)*(ny-1)*nz),
-     +  F1(nx*(ny-1)*(nz-1)),F2((nx-1)*ny*(nz-1))
-     
 ! ===================================================================
 ! Title: makeRHS.f 
 ! Authors: N. Vilanakis, E. Mathioudakis
@@ -51,10 +30,33 @@
 ! e3: complex array, i*omega*m0*SIGMAEZ, dimension: (nx-1)*(ny-1)*nz
 ! F1: complex array, Right-hand side of equation 1 (refers to Ex nodes), dimension: nx*(ny-1)*(nz-1)
 ! F2: complex array, Right-hand side of equation 1 (refers to Ey nodes), dimension: (nx-1)*ny*(nz-1) 
+!====================================================================  
+      subroutine makeRHS(nx,ny,nz,omega,m0,ci,wpath,
+     +        SIGMAEX,SIGMAEY,SIGMAEZ,SIGMADiffEX,SIGMADiffEY,
+     +        realExBack,imagExBack,realEyBack,imagEyBack,ExBack,EyBack,
+     +        e1,e2,e3,F1,F2)
+
+      implicit none
+      integer:: nx,ny,nz,i
+      character(64) :: wpath
+      real*8:: omega,m0,dznrm2,
+     +          SIGMAEX(nx*(ny-1)*(nz-1)),SIGMADiffEX(nx*(ny-1)*(nz-1)),
+     +          SIGMAEY((nx-1)*ny*(nz-1)),SIGMADiffEY((nx-1)*ny*(nz-1)),
+     +          SIGMAEZ((nx-1)*(ny-1)*nz),
+     +          realExBack(nx*(ny-1)*(nz-1)),
+     +          imagExBack(nx*(ny-1)*(nz-1)),
+     +          realEyBack((nx-1)*ny*(nz-1)),
+     +          imagEyBack((nx-1)*ny*(nz-1))
+ 
+      complex*16:: ci,ExBack(nx*(ny-1)*(nz-1)),EyBack((nx-1)*ny*(nz-1)),
+     +  e1(nx*(ny-1)*(nz-1)),e2((nx-1)*ny*(nz-1)),e3((nx-1)*(ny-1)*nz),
+     +  F1(nx*(ny-1)*(nz-1)),F2((nx-1)*ny*(nz-1))
+
 !==================================================================== 
-
-
-
+! Open .txt created by MATLAB create_EbackFiles script
+! and decouple real and imaginary part of the electric 
+! field components
+!====================================================================  
          open(10,file=trim(wpath)//'temp/ExBack.txt')
          open(11,file=trim(wpath)//'temp/EyBack.txt')
          do i=1,nx*(ny-1)*(nz-1)
@@ -68,33 +70,42 @@
          close(10)
          close(11)
          print*,'ExBack - EyBack copied'
-
-!       Compute F1
+!==================================================================== 
+! Compute F1 - RHS of equation AEx+BEy+CEz=F1
+!====================================================================  
 !$OMP PARALLEL
 !$OMP DO
       do i=1,nx*(ny-1)*(nz-1)
        F1(i)=-ci*omega*m0*SIGMADiffEX(i)*ExBack(i)
       enddo
 !$OMP END DO
-!       Compute F2
+!====================================================================  
+! Compute F2 - RHS of equation DEx+ZEy+KEz=F2
+!====================================================================  
 !$OMP DO
       do i=1,(nx-1)*ny*(nz-1)
        F2(i)=-ci*omega*m0*SIGMADiffEY(i)*EyBack(i)
       enddo
 !$OMP END DO
-!       Compute e1 coefficient used in subroutine AS.f
+!====================================================================  
+! Compute e1 coefficient used in subroutine AS.f
+!====================================================================  
 !$OMP DO
       do i=1,nx*(ny-1)*(nz-1)
        e1(i) = ci*omega*m0*SIGMAEX(i)
       enddo
 !$OMP END DO
-!       Compute e2 coefficient used in subroutine ZS.f
+!====================================================================  
+! Compute e2 coefficient used in subroutine ZS.f
+!====================================================================  
 !$OMP DO
       do i=1,(nx-1)*ny*(nz-1)
        e2(i) = ci*omega*m0*SIGMAEY(i)
       enddo
 !$OMP END DO
-!       Compute e3 coefficient used in subroutine PS.f
+!====================================================================  
+! Compute e3 coefficient used in subroutine PS.f
+!====================================================================  
 !$OMP DO
       do i=1,(nx-1)*(ny-1)*nz
        e3(i) = ci*omega*m0*SIGMAEZ(i)
