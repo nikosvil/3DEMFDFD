0885825ca48808ef031a1d238ee41b9959ae137d
Nikos <nikosvil@hotmail.com>
2023-05-16 10:23:46 +0300

    Update NS.f

--- halfspace/NS.f
+++ halfspace/NS.f
@@ -1,7 +1,10 @@
 ! ===================================================================
 ! Title: NS.f 
-! Authors: N. Vilanakis, E. Mathioudakis
+! Authors: N. D. Vilanakis, E. N. Mathioudakis
 ! Details: Applied Mathematics and Computers Lab, Technical University of Crete
+! * A 3D frequency-domain electromagnetic solver employing a high order compact finite-difference scheme
+! * N. D. Vilanakis, N. Economou, E. N. Mathioudakis, A. Vafidis
+! * Computers and Geosciences
 !====================================================================
 ! NS.f performs the multiplication N*q=tempEZ
 ! by performing the operations between terms that make up block N 
