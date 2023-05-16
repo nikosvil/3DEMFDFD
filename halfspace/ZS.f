0ecc6c8f3c866e3ae0aae9b19e0e3092d751c0bf
Nikos <nikosvil@hotmail.com>
2023-05-16 10:27:48 +0300

    Update ZS.f

--- halfspace/ZS.f
+++ halfspace/ZS.f
@@ -1,7 +1,10 @@
 ! ===================================================================
 ! Title: ZS.f 
-! Authors: N. Vilanakis, E. Mathioudakis
+! Authors: N. D. Vilanakis, E. N. Mathioudakis
 ! Details: Applied Mathematics and Computers Lab, Technical University of Crete
+! * A 3D frequency-domain electromagnetic solver employing a high order compact finite-difference scheme
+! * N. D. Vilanakis, N. Economou, E. N. Mathioudakis, A. Vafidis
+! * Computers and Geosciences
 !====================================================================
 ! ZS.f performs the multiplication Z*q=tempEY
 ! by performing the operations between terms that make up block Z 
