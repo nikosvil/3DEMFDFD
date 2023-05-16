a1c01c3442663a8109b9abdc9b0aaefefe5bba5b
Nikos <nikosvil@hotmail.com>
2023-05-16 10:23:34 +0300

    Update MS.f

--- halfspace/MS.f
+++ halfspace/MS.f
@@ -1,7 +1,10 @@
 ! ===================================================================
 ! Title: MS.f 
-! Authors: N. Vilanakis, E. Mathioudakis
+! Authors: N. D. Vilanakis, E. N. Mathioudakis
 ! Details: Applied Mathematics and Computers Lab, Technical University of Crete
+! * A 3D frequency-domain electromagnetic solver employing a high order compact finite-difference scheme
+! * N. D. Vilanakis, N. Economou, E. N. Mathioudakis, A. Vafidis
+! * Computers and Geosciences
 !====================================================================
 ! MS.f performs the multiplication M*q=tempEZ
 ! by performing the operations between terms that make up block M 
