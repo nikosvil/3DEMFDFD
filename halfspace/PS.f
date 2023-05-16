e27c9c3119dca6655b51b16c4b1343e0ba334886
Nikos <nikosvil@hotmail.com>
2023-05-16 10:27:17 +0300

    Update PS.f

--- halfspace/PS.f
+++ halfspace/PS.f
@@ -1,7 +1,10 @@
 ! ===================================================================
 ! Title: PS.f 
-! Authors: N. Vilanakis, E. Mathioudakis
+! Authors: N. D. Vilanakis, E. N. Mathioudakis
 ! Details: Applied Mathematics and Computers Lab, Technical University of Crete
+! * A 3D frequency-domain electromagnetic solver employing a high order compact finite-difference scheme
+! * N. D. Vilanakis, N. Economou, E. N. Mathioudakis, A. Vafidis
+! * Computers and Geosciences
 !====================================================================
 ! PS.f performs the multiplication P*q=tempEZ
 ! by performing the operations between terms that make up block P 
